e280cdad6a2d503246ecaf40c54e4a9b2fcc11ba
Jessica Gestwicki <gestwicki@gmail.com>
2021-01-12 15:38:53 -0500

    Try and remove toggle fullscreen on Main

--- project/src/Main.gd
+++ project/src/Main.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 export var background_scroll_speed := 75.0
 
 
+
 func _process(delta):
 	$ParallaxBackground.scroll_offset.x -= background_scroll_speed * delta
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
