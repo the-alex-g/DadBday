059ff7d38170a2676d2fc06148dce77f37761408
Alex Gestwicki <alex.gestwicki@gmail.com>
2021-01-12 10:29:39 -0500

    Attempt to fix not-going-fullscreen problem #2

--- project/src/Main.gd
+++ project/src/Main.gd
@@ -8,9 +8,11 @@ export var background_scroll_speed := 75.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.action_press("Fakeout")
 
 func _process(delta):
+	if Input.is_action_just_pressed("Fakeout"):
+		$AnimationPlayer/AudioStreamPlayer.play()
 	$ParallaxBackground.scroll_offset.x -= background_scroll_speed * delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- project/src/SplashScreen.gd
+++ project/src/SplashScreen.gd
@@ -3,8 +3,12 @@ extends Control
 
 func _ready():
 	Input.action_press("Fakeout")
-	OS.window_fullscreen = true
 	$AnimationPlayer.play("text")
 
+func _process(_delta):
+	if Input.is_action_just_pressed("Fakeout"):
+		OS.window_fullscreen = true
+
 func _on_AnimationPlayer_animation_finished(anim_name):
+	Input.action_release("Fakeout")
 	var _error = get_tree().change_scene("res://src/Main.tscn")
