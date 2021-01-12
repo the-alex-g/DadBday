eaa3fc2b0291632afe2047b34934f60d8b2c8c44
Alex Gestwicki <alex.gestwicki@gmail.com>
2021-01-12 10:33:54 -0500

    Attempt to fix not-going-fullscreen problem #3

--- project/src/SplashScreen.gd
+++ project/src/SplashScreen.gd
@@ -8,7 +8,7 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("Fakeout"):
 		OS.window_fullscreen = true
+		Input.action_release("Fakeout")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	Input.action_release("Fakeout")
 	var _error = get_tree().change_scene("res://src/Main.tscn")
