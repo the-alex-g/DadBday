efbf85e0c4a753660e4b104da94310db13be9c63
Alex Gestwicki <alex.gestwicki@gmail.com>
2021-01-12 08:36:49 -0500

    Add splashscreen animation and go to next scene after animation completed

--- project/src/SplashScreen.gd
+++ project/src/SplashScreen.gd
@@ -1,7 +1,9 @@
 extends Control
 
 
-func _input(event):
-	if event is InputEventMouseButton:
-		OS.window_fullscreen = true
-		var _error = get_tree().change_scene("res://src/Main.tscn")
+func _ready():
+	$AnimationPlayer.play("text")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	OS.window_fullscreen = true
+	var _error = get_tree().change_scene("res://src/Main.tscn")
